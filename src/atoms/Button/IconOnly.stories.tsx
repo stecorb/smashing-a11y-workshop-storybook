bd15b74a5a983cbe2f3af0ef4f57e279f802bf1f
Stefan Corbin <stefan.corbin@gc-gruppe.de>
2026-09-10 14:15:11 +0200

    add accessibility checks

--- src/atoms/Button/IconOnly.stories.tsx
+++ src/atoms/Button/IconOnly.stories.tsx
@@ -5,6 +5,12 @@ import { Button } from './Button';
 const meta = {
   title: 'Broken/Icon Button',
   component: Button,
+  parameters: {
+    a11y: {
+      // I have good reasons for that, trust me. - Sep 10, 2026
+      test: 'todo',
+    },
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
